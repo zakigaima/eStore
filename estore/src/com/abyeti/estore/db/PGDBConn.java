2834f655f28ca2e646ba2ad239cec0aedddad7f6
Zaki Gaima <zakigaima@yahoo.co.in>
2015-01-07 12:48:43 +0530

    Data from database converted to JSON format

---
+++ estore/src/com/abyeti/estore/db/PGDBConn.java
@@ -0,0 +1,26 @@
+package com.abyeti.estore.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PGDBConn {
+	
+	public static String DRIVER_NAME = "org.postgresql.Driver";
+	public static String URL = "jdbc:postgresql://localhost:5432/PGDemo";
+	public static String USERNAME = "postgres";
+	public static String PASSWORD = "654321";
+	public static Connection conn = null;
+
+    public static Connection dbConnection() throws Exception {
+		
+		try{
+		    Class.forName(DRIVER_NAME);
+			conn = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+			if(conn != null)
+				return conn;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
---
+++ estore/src/com/abyeti/estore/inventory/V1_inventory.java
@@ -0,0 +1,53 @@
+package com.abyeti.estore.inventory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.abyeti.estore.db.*;
+import com.abyeti.util.ToJSON;
+
+@Path("/v1/inventory")
+public class V1_inventory {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String returnAllPcParts() throws Exception {
+		
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		
+		try {
+			conn = PGDBConn.dbConnection();
+			query = conn.prepareStatement("select * from employee");
+			
+			ResultSet rs = query.executeQuery();
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			query.close(); //close connection
+			
+			returnString = json.toString();
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			 if (conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+	
+}
